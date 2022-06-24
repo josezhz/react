a62713ddf11fbffad83d2c90c8ba21d3fe9aa708
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-06-24 07:48:04 +0000

    Done handson-1.1

--- handson-1.1/src/App.js
+++ handson-1.1/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css'
 
 function App() {
   return (
     <React.Fragment>
-      
+      <nav>
+        <ul id="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Our Menu</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+      <section id="hero">
+        <div id="cta">
+          <a href="#">
+            Make a Booking
+          </a>
+        </div>
+      </section>
+      <section id="about-us">
+        <div class="content">
+          <h1>Japanese so fresh, any fresh you have to fly to Japan</h1>
+        </div>
+      </section>
     </React.Fragment>
   );
 }
