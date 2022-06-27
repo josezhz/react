9427c16ae796e79f2f06b00b2d2c60f41b2d354b
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-06-27 03:42:30 +0000

    Done 03-states

--- 03-states/src/App.js
+++ 03-states/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NumberBox from './components/NumberBox'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NumberBox initialValue={1} />
+      <NumberBox initialValue={2} />
+      <NumberBox initialValue={3} />
+      <NumberBox initialValue={4} />
+    </React.Fragment>
   );
 }
 
